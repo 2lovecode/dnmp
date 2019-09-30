92426a3ef37f487e4118530ec83451d90879150e
liu hao <liu546hao@163.com>
2019-09-30 11:32:52 +0800

    edit

--- app/index.php
+++ app/index.php
@@ -1,2 +1,19 @@
 <?php
-  phpinfo();
+
+echo "Tool List";
+?>
+
+<ul>
+	<li>
+		<a href="./adminer/adminer-4.7.2.php">Adminer</a>
+	</li>
+	<li>
+		<a href="./yii-docs-2.0-zh-CN/guide-README.html">Yii Guide</a>
+	</li>
+	<li>
+		<a href="./yii-docs-2.0-zh-CN/index.html">Yii Reference</a>
+	</li>
+	<li>
+		<a href="./phpinfo.php">phpinfo</a>
+	</li>
+</ul>
