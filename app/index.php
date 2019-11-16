379c1851fbc459b18ffee801c9a54ce21435e7f8
2lovecode <liu546hao@163.com>
2019-11-16 23:25:30 +0800

    添加工具

--- app/index.php
+++ app/index.php
@@ -1,19 +1,19 @@
 <?php
 
-echo "Tool List";
+echo "Tools List";
 ?>
 
 <ul>
 	<li>
-		<a href="./adminer/adminer-4.7.2.php">Adminer</a>
+		<a href="./tools/phpinfo.php">Phpinfo</a>
 	</li>
 	<li>
-		<a href="./yii-docs-2.0-zh-CN/guide-README.html">Yii Guide</a>
+		<a href="./tools/adminer/adminer-4.7.5.php">Adminer-4.7.5[数据库管理工具]</a>
 	</li>
 	<li>
-		<a href="./yii-docs-2.0-zh-CN/index.html">Yii Reference</a>
+		<a href="./tools/Yii/yii-docs-2.0-zh-CN/guide-README.html">Yii Guide-2.0</a>
 	</li>
 	<li>
-		<a href="./phpinfo.php">phpinfo</a>
+		<a href="./tools/Yii/yii-docs-2.0-zh-CN/index.html">Yii Reference-2.0</a>
 	</li>
 </ul>
